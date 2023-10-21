3cd8df0040e1b8163d1d9b5285b0bce7c01c36e9
DaoDat <daonguyendat01637@gmail.com>
2023-10-21 20:52:26 +0700

    Add project files.

---
+++ CinemaWeb/Scripts/MyJS/Admin.js
@@ -0,0 +1,19 @@
+﻿var overlay = document.querySelector('.overlay');
+var formadd = document.querySelector('.form-add');
+var links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        links.forEach(element => {
+            element.classList.remove('active');
+        })
+        e.target.classList.add('active');
+    })
+});
+function turnoff() {
+    overlay.style.display = 'none';
+    formadd.style.display = 'none';
+}
+function turnon() {
+    overlay.style.display = 'block';
+    formadd.style.display = 'block';
+}
